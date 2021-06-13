8a5904f2d0a63f4f51c6b3fadc00765f02f4bd15
francescocm <82612645+francescocm@users.noreply.github.com>
2021-06-12 21:29:32 -0300

    Add files via upload

---
+++ desafiofunciones.js
@@ -0,0 +1,34 @@
+function contenido(id) {
+    return document.getElementById(id);
+}
+
+function calcularProme() {
+    var pn = parseInt(contenido("1nota").value);
+    var sn = parseInt(contenido("2nota").value);
+    var tn = parseInt(contenido("3nota").value);
+    var cn = parseInt(contenido("4nota").value);
+    var total = (pn + sn + tn + cn)/4;
+    contenido("prom").value = total;
+
+    if (total >=9 & total <=10){
+        contenido("EQ").value = "Leyenda";
+    }
+    else if (total >=8 & total <= 9){
+        contenido("EQ").value = "Clase Mundial";
+    }
+    else if (total >=7 & total <= 8){
+        contenido("EQ").value = "Semi-Profesional";
+    }
+    else if (total >=6 & total <= 7){
+        contenido("EQ").value = "Profesional";
+    }
+    else if (total >=4 & total <= 6){
+        contenido("EQ").value = "Amateur";
+    }
+    //else if (total >6){
+    //    contenido("EQ").value = "Baby player";
+    // }
+    else {
+        contenido("EQ").value = "Manco";
+    }
+}
